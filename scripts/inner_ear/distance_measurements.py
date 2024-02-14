d5cf40ede34365a42f063454160d66049d36da65
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-02-14 10:26:02 +0100

    Fix issues in distance visualization for ribbon synapses

--- scripts/inner_ear/distance_measurements.py
+++ scripts/inner_ear/distance_measurements.py
@@ -9,9 +9,7 @@ import pandas
 from elf.io import open_file
 
 from synaptic_reconstruction.distance_measurements import (
-    measure_pairwise_object_distances,
     measure_segmentation_to_object_distances,
-    create_distance_lines,
     create_object_distance_lines,
     filter_blocked_segmentation_to_object_distances,
 )
@@ -31,14 +29,18 @@ def compute_all_distances(vesicles, ribbon, pd, boundaries, resolution, save_fol
     # Compute the distance of the vesicle to the boundaries.
     boundary_save = os.path.join(save_folder, "boundary_dist.npz")
     if not os.path.exists(boundary_save):
-        measure_segmentation_to_object_distances(vesicles, boundaries, save_path=boundary_save, resolution=resolution)
+        measure_segmentation_to_object_distances(
+            vesicles, boundaries, save_path=boundary_save, resolution=resolution
+        )
 
-    # Compute the distance between all the vesicles.
-    ves_save = os.path.join(save_folder, "ves_dist.npz")
-    if not os.path.exists(ves_save):
-        measure_pairwise_object_distances(vesicles, save_path=ves_save, n_threads=2, resolution=resolution)
+    # NOTE: we do not need the inter-vesicle distances
+    # # Compute the distance between all the vesicles.
+    # ves_save = os.path.join(save_folder, "ves_dist.npz")
+    # if not os.path.exists(ves_save):
+    #     measure_pairwise_object_distances(vesicles, save_path=ves_save, n_threads=2, resolution=resolution)
 
-    return ves_save, ribbon_save, pd_save, boundary_save
+    # return ves_save, ribbon_save, pd_save, boundary_save
+    return ribbon_save, pd_save, boundary_save
 
 
 def _get_data(tomo_path):
@@ -128,7 +130,7 @@ def assign_vesicles_to_pools(
 
 def visualize_distances(
     tomo, vesicles, ribbon, pd, boundaries,
-    distance_path_vesicles, distance_path_ribbon, distance_path_pd, distance_path_boundaries,
+    distance_path_ribbon, distance_path_pd, distance_path_boundaries,
     scale=2, show=True,
 ):
     import napari
@@ -144,18 +146,21 @@ def visualize_distances(
     vesicle_ids, pool_assignments = assign_vesicles_to_pools(
         vesicles, distance_path_ribbon, distance_path_pd, distance_path_boundaries, scale=scale
     )
-    # TODO for which pairs of vesicles do we measure distances? Only RA-V to RA-V etc. or between all pools?
-    # Do we need to take into account blocking by the ribbon, or do we measure distances across the ribbon?
-    ves_lines, ves_props = create_distance_lines(distance_path_vesicles, n_neighbors=3, scale=scale)
-    # TODO which distances are of interest? (All vesicles or only RA-V?)
-    ribbon_lines, ribbon_props = create_object_distance_lines(distance_path_ribbon, seg_ids=vesicle_ids, scale=scale)
-    # TODO which distances are of interest? (All vesicles or only MP-V + Docked-V?)
+    #
+    # ves_lines, ves_props = create_distance_lines(distance_path_vesicles, n_neighbors=3, scale=scale)
+
+    # Only the RA-V distances
+    ribbon_lines, ribbon_props = create_object_distance_lines(
+        distance_path_ribbon, seg_ids=vesicle_ids, scale=scale
+    )
+
+    # Only the MP-V and docked distances
     pd_lines, pd_props = create_object_distance_lines(distance_path_pd, seg_ids=vesicle_ids, scale=scale)
     boundary_lines, boundary_props = create_object_distance_lines(
         distance_path_boundaries, seg_ids=vesicle_ids, scale=scale
     )
 
-    ves_dist = pandas.DataFrame(ves_props)
+    # ves_dist = pandas.DataFrame(ves_props)
     ribbon_dist = pandas.DataFrame(ribbon_props)
     pd_dist = pandas.DataFrame(pd_props)
     boundary_dist = pandas.DataFrame(boundary_props)
@@ -166,7 +171,7 @@ def visualize_distances(
         }
     )
     if not show:
-        return ves_assignments, ves_dist, ribbon_dist, pd_dist, boundary_dist
+        return ves_assignments, ribbon_dist, pd_dist, boundary_dist
 
     vesicle_pools = np.zeros_like(vesicles)
     for pool_id, pool_name in enumerate(("RA-V", "MP-V", "Docked-V"), 1):
@@ -181,20 +186,19 @@ def visualize_distances(
     v.add_labels(pd, name="presynaptic-density")
     v.add_labels(boundaries)
 
-    v.add_shapes(ves_lines, shape_type="line", name="vesicle-distances", visible=False)
+    # v.add_shapes(ves_lines, shape_type="line", name="vesicle-distances", visible=False)
     v.add_shapes(ribbon_lines, shape_type="line", name="ribbon-distances", visible=False)
     v.add_shapes(pd_lines, shape_type="line", name="presynaptic-distances", visible=False)
     v.add_shapes(boundary_lines, shape_type="line", name="boundary-distances", visible=False)
 
     napari.run()
 
-    return ves_assignments, ves_dist, ribbon_dist, pd_dist, boundary_dist
+    return ves_assignments, ribbon_dist, pd_dist, boundary_dist
 
 
-def to_excel(ves_assignments, vesicle_distances, ribbon_distances, pd_distances, boundary_distances, result_path):
+def to_excel(ves_assignments, ribbon_distances, pd_distances, boundary_distances, result_path):
     ves_assignments.to_excel(result_path, sheet_name="vesicle-pools", index=False)
     with pandas.ExcelWriter(result_path, engine="openpyxl", mode="a") as writer:
-        vesicle_distances.to_excel(writer, sheet_name="vesicle-vesicle-distances", index=False)
         ribbon_distances.to_excel(writer, sheet_name="vesicle-ribbon-distances", index=False)
         pd_distances.to_excel(writer, sheet_name="vesicle-pd-distances", index=False)
         boundary_distances.to_excel(writer, sheet_name="vesicle-boundary-distances", index=False)
@@ -216,23 +220,23 @@ def process_distance_measurements(tomo_path, show):
 
     distance_save_folder = os.path.join(os.path.split(tomo_path)[0], "distances")
     os.makedirs(distance_save_folder, exist_ok=True)
-    ves_dist, ribbon_dist, pd_dist, boundary_dist = compute_all_distances(
+    ribbon_dist, pd_dist, boundary_dist = compute_all_distances(
         vesicles, ribbon, pd, boundaries, resolution, distance_save_folder
     )
 
     with open_file(tomo_path, "r") as f:
         tomo = f["data"][:]
-    ves_assignments, ves_dist, ribbon_dist, pd_dist, boundary_dist = visualize_distances(
-        tomo, vesicles, ribbon, pd, boundaries, ves_dist, ribbon_dist, boundary_dist, pd_dist, show=show
+    ves_assignments, ribbon_dist, pd_dist, boundary_dist = visualize_distances(
+        tomo, vesicles, ribbon, pd, boundaries, ribbon_dist, pd_dist, boundary_dist, show=show
     )
 
     out_path = os.path.join(distance_save_folder, "distances.xlsx")
-    to_excel(ves_assignments, ves_dist, ribbon_dist, pd_dist, boundary_dist, result_path=out_path)
+    to_excel(ves_assignments, ribbon_dist, pd_dist, boundary_dist, result_path=out_path)
 
 
+# FIXME check the distances, they are not showing the correct ones for PD and Boundaries (appear to be switched)
 def main():
     # path on laptop
-    # tomo_path = "/home/pape/Work/data/moser/em-susi/04_wild_type_strong_stimulation/NichtAnnotiert/M1aModiolar/2/Emb71M1aGridA4sec4mod6.rec"
     parser = argparse.ArgumentParser()
     parser.add_argument("tomo_path")
     parser.add_argument("-s", "--show", action="store_true")
