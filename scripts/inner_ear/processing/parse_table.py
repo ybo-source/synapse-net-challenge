e4cb1545f14213106480464e94a2980592fc9421
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-03-17 10:00:36 +0100

    Implement table parsing for inner ear experiments

--- scripts/inner_ear/processing/parse_table.py
+++ scripts/inner_ear/processing/parse_table.py
@@ -1,5 +1,50 @@
+import os
+import warnings
+
+import numpy as np
 import pandas as pd
 
-table = "/home/pape/Downloads/Übersicht.xlsx"
 
-tab = pd.read_excel(table)
+def parse_table(table_path, data_root, require_local_path=True):
+    table = pd.read_excel(table_path)
+    local_paths = []
+
+    prefix = "Electron-Microscopy-Susi/Analyse"
+    for _, row in table.iterrows():
+        # Skip empty rows.
+        if row.Bedingung:
+            local_paths.append("")
+            continue
+        if isinstance(row.Bedingung, float) and np.isnan(row.Bedingung):
+            local_paths.append("")
+            continue
+
+        try:
+            path = os.path.join(
+                data_root, prefix, row.Bedingung, f"Mouse {int(row.Maus)}",
+                row["Ribbon-Orientierung"].lower(), str(int(row["OwnCloud-Unterordner"]))
+            )
+        except ValueError as e:
+            print(row)
+            raise e
+
+        if not os.path.exists(path):
+            if require_local_path:
+                raise RuntimeError(f"Cannot find {path}")
+            else:
+                warnings.warn(f"Cannot find {path}")
+        local_paths.append(path)
+
+    assert len(local_paths) == len(table)
+    table["Local Path"] = local_paths
+    return table
+
+
+def main():
+    table_path = "./Übersicht.xlsx"
+    data_root = "/scratch-emmy/usr/nimcpape/data/moser"
+    parse_table(table_path, data_root)
+
+
+if __name__ == "__main__":
+    main()
