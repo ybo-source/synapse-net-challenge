4910da3d8fa58d87ce2522b1ef3e54138b8f9527
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-07-14 20:28:53 +0200

    Update tomogram rescaling script

--- scripts/inner_ear/processing/rescale_tomogram.py
+++ scripts/inner_ear/processing/rescale_tomogram.py
@@ -13,33 +13,48 @@ def rescale_tomogram(folder, factor=2):
     tomo_path = get_data_path(folder)
     with mrcfile.open(tomo_path) as f:
         tomo = f.data[:]
-        voxel_size = f.voxel_size
-
-    breakpoint()
+        voxel_size = f.voxel_size.tolist()
     scale_factor = 1.0 / factor
-    tomo = rescale(tomo, scale_factor, order=3, preserve_range=True).astype(tomo.dtype)
-    voxel_size = voxel_size * factor
-    breakpoint()
 
-    mrcfile.write(tomo_path, tomo, voxel_size=voxel_size)
+    tomo = rescale(tomo, scale_factor, order=3, preserve_range=True).astype(tomo.dtype)
+    voxel_size = [vx * factor for vx in voxel_size]
+    mrcfile.write(tomo_path, tomo, voxel_size=voxel_size, overwrite=True)
 
     # rescale the segmentations
     segmentation_files = glob(os.path.join(folder, "automatisch", "v2", "*.h5"))
     for seg_file in segmentation_files:
         with h5py.File(seg_file, "r") as f:
             seg = f["segmentation"][:]
-            pred = f["prediction"][:]
+
+            if seg.shape[0] <= tomo.shape[0]:
+                continue
+            print("Rescaling", seg_file)
+
+            if "prediction" in f:
+                pred = f["prediction"][:]
+            else:
+                pred = None
+
         seg = rescale(tomo, scale_factor, order=0, anti_aliasing=False, preserve_range=True).astype(seg.dtype)
-        pred = rescale(tomo, scale_factor, order=0, anti_aliasing=False, preserve_range=True).astype(pred.dtype)
+        assert seg.shape == tomo.shape
+        if pred is not None:
+            pred = rescale(tomo, scale_factor, order=0, anti_aliasing=False, preserve_range=True).astype(pred.dtype)
+            assert pred.shape == tomo.shape
+
         with h5py.File(seg_file, "w") as f:
             f.create_dataset("segmentation", data=seg, compression="gzip")
-            f.create_dataset("prediction", data=pred, compression="gzip")
+            if pred is not None:
+                f.create_dataset("prediction", data=pred, compression="gzip")
 
 
 def main():
     root = get_data_root()
+
     folder1 = os.path.join(root, "Electron-Microscopy-Susi/Analyse/WT control/Mouse 1/pillar/3")
     rescale_tomogram(folder1)
+
+    folder2 = os.path.join(root, "Electron-Microscopy-Susi/Analyse/WT control/Mouse 1/pillar/4")
+    rescale_tomogram(folder2)
 
 
 if __name__ == "__main__":
