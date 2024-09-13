61242bdc0068dda7ebe03d5653f4b80faa237125
SarahMuth <sarahmuth9@gmail.com>
2024-09-13 11:20:16 +0200

    first implementation of postprocessing vesicle shape seg

--- scripts/cooper/ground_truth/vesicles/vesicle_postprocessing.py
+++ scripts/cooper/ground_truth/vesicles/vesicle_postprocessing.py
@@ -6,29 +6,94 @@ import numpy as np
 
 from elf.evaluation.matching import label_overlap, intersection_over_union
 from skimage.segmentation import relabel_sequential
-from scipy.ndimage import binary_dilation
+from scipy.ndimage import binary_dilation, binary_erosion, distance_transform_edt
 from tqdm import tqdm
+
+from skimage.morphology import ball
 
 from synaptic_reconstruction.inference.vesicles import segment_vesicles
 from synaptic_reconstruction.inference.util import _get_file_paths
 
+from vigra.analysis import watershedsNew
+from skimage.segmentation import watershed
+
 MODEL_PATH = "/scratch-grete/projects/nim00007/data/synaptic_reconstruction/models/cooper/vesicles/3D-UNet-for-Vesicle-Segmentation-vesicles-010508model_v1r45_0105mr45_0105mr45.zip"  # noqa
 
 
-def extract_gt_bounding_box(raw, vesicle_gt, halo=[2, 32, 32]):
+def extract_gt_bounding_box(raw, vesicle_gt, coordinates, halo=[2, 32, 32]):
     bb = np.where(vesicle_gt > 0)
     bb = tuple(slice(
         max(int(b.min() - ha), 0),
         min(int(b.max()) + ha, sh)
     ) for b, sh, ha in zip(bb, raw.shape, halo))
     raw, vesicle_gt = raw[bb], vesicle_gt[bb]
-    return raw, vesicle_gt
+
+    #TODO check if offset is needed
+    offset = np.array([b.start for b in bb])[None]
+    coordinates -= offset
+
+    seed_ids = np.arange(1, len(coordinates) + 1)
+    coordinates = tuple(coordinates[:, i] for i in range(coordinates.shape[1]))
+
+    return raw, vesicle_gt, coordinates, seed_ids
 
 
-# TODO
 # Postprocess the vesicle shape (if still necessary after fixing the IMOD extraction).
-def postprocess_vesicle_shape(vesicle_gt, prediction):
-    return vesicle_gt
+def postprocess_vesicle_shape(vesicle_gt, prediction, coordinates, radii, seed_ids):
+    _, boundaries = prediction.squeeze()
+    boundary_threshold = 0.05
+    distance_map = distance_transform_edt(boundaries < boundary_threshold)
+    distance_map = 1. - (distance_map - distance_map.min()) / distance_map.max()
+    distance_map += boundaries
+
+    #TODO edge filter??
+
+    shape = distance_map.shape
+    labels_pp = np.zeros_like(distance_map, dtype="uint32")
+    coordinates = [
+        (coordinates[0][i], coordinates[1][i], coordinates[2][i])
+        for i in range(len(coordinates[0]))
+    ]
+
+    assert len(seed_ids) == len(coordinates) == len(radii)
+    for label_id, coord, radius in zip(seed_ids, coordinates, radii):
+        radius = int(radius)
+        roi = tuple(
+            slice(max(co - radius, 0), min(co + radius, sh)) for co, sh in zip(coord, shape)
+        )
+
+        #handeling the case where part of the sphere mask would go out of bounds
+        radius_clipped_left = [co - max(co - radius, 0) for co in coord]
+        radius_clipped_right = [min(co + radius, sh) - co for co, sh in zip(coord, shape)]
+        mask_slice = tuple(
+            slice(radius + 1 - rl, radius + 1 + rr) for rl, rr in zip(radius_clipped_left, radius_clipped_right)
+        )
+
+        mask = ball(radius)[mask_slice]
+
+        dist = distance_map[roi]
+        assert mask.shape == dist.shape
+
+        #TODO? change dilation as well?
+        bg_mask_dilation=2
+        bg_mask = ~binary_dilation(mask, iterations=bg_mask_dilation)
+
+        #TODO decide on seed erosion
+        seed_erosion = 5
+        fg_mask = binary_erosion(mask, iterations=seed_erosion)
+        centroid = tuple(co - b.start for co, b in zip(coord, roi))
+        fg_mask[centroid] = 1
+
+        seeds = np.zeros_like(mask, dtype="uint32")
+        seeds[fg_mask] = 1
+        seeds[bg_mask] = 2
+        #TODO choose between watershedsNews and from skimage.segmentation import watershed
+        #pp = watershedsNew(dist.astype("float32"), seeds=seeds)[0]
+        pp = watershed(-dist, markers=seeds, mask=mask)
+        
+        labels_pp[roi][pp == 1] = label_id
+
+    return labels_pp
 
 
 def find_additional_vesicles(vesicle_gt, segmentation, matching_threshold=0.5):
@@ -57,7 +122,7 @@ def find_additional_vesicles(vesicle_gt, segmentation, matching_threshold=0.5):
     return additional_vesicles
 
 
-def postprocess_vesicle_gt(raw, vesicle_gt):
+def postprocess_vesicle_gt(raw, vesicle_gt, coordinates, radii):
     """Run post-processing for the vesicle ground-truth extracted from IMOD.
     This includes the following steps:
     - Extract the bounding box around the annotated vesicles in the ground-truth.
@@ -71,13 +136,13 @@ def postprocess_vesicle_gt(raw, vesicle_gt):
     assert raw.shape == vesicle_gt.shape
 
     # Extract the bounding box of the data that contains the vesicles in the GT.
-    raw, vesicle_gt = extract_gt_bounding_box(raw, vesicle_gt)
+    raw, vesicle_gt, coordinates, seed_ids = extract_gt_bounding_box(raw, vesicle_gt, coordinates)
 
     # Get the model predictions and segmentation for this data.
     segmentation, prediction = segment_vesicles(raw, MODEL_PATH, return_predictions=True)
 
     # Additional post-processing to improve the shape of the vesicles.
-    vesicle_gt = postprocess_vesicle_shape(vesicle_gt, prediction)
+    vesicle_gt = postprocess_vesicle_shape(vesicle_gt, prediction, coordinates, radii, seed_ids)
 
     # Get vesicles in the prediction that are not part of the ground-truth.
     additional_vesicles = find_additional_vesicles(vesicle_gt, segmentation)
@@ -99,9 +164,11 @@ def create_vesicle_ground_truth_versions(input_path, output_path, gt_key):
     with h5py.File(input_path, "r") as f:
         raw = f["raw"][:]
         vesicle_gt = f[gt_key][:]
+        coordinates = f["labels/imod/vesicles/coordinates"][:]
+        radii = f["labels/imod/vesicles/radii"][:]
 
     # Extract raw data, vesicle gt and additional vesicles from the gt bounding box.
-    raw, vesicle_gt, additional_vesicles = postprocess_vesicle_gt(raw, vesicle_gt)
+    raw, vesicle_gt, additional_vesicles = postprocess_vesicle_gt(raw, vesicle_gt, coordinates, radii)
 
     # Create a new ground-truth version where all additional vesicles are masked out with -1.
     masked_vesicles = _mask_additional_vesicles(vesicle_gt, additional_vesicles)
