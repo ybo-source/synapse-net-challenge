80be9ba538b3ad5885aeb3665b2ca18e16b2ab89
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-09-24 11:13:26 +0200

    Update vesicle extraction for cryo data

--- scripts/cryo/vesicles/extract_vesicle_gt.py
+++ scripts/cryo/vesicles/extract_vesicle_gt.py
@@ -1,11 +1,12 @@
 import os
+from pathlib import Path
 
 import imageio.v3 as imageio
 import napari
 import numpy as np
 
 from elf.io import open_file
-from scipy.ndimage import distance_transform_edt, binary_closing
+from scipy.ndimage import distance_transform_edt, binary_closing, binary_erosion
 from skimage.measure import label, regionprops
 from skimage.segmentation import watershed, find_boundaries
 from tqdm import trange, tqdm
@@ -40,7 +41,7 @@ def make_instances(boundaries, apply_closing=True):
 
     eps = 1e-6
     for z in trange(seg.shape[0], desc="Processing slices"):
-        bd = boundaries[z]
+        bd = binary_erosion(boundaries[z], iterations=1)
         props = regionprops(seed_map[z])
         seeds = np.zeros(bd.shape, dtype="int16")
         for prop in props:
@@ -68,7 +69,7 @@ def make_instances(boundaries, apply_closing=True):
     return seg
 
 
-def extract_vesicles(raw_file, seg_file, correction_file, apply_closing=True):
+def extract_vesicles(raw_file, seg_file, correction_file, apply_closing=True, output_path=None):
     with open_file(raw_file, "r") as f:
         raw = f["data"][:]
     with open_file(seg_file, "r") as f:
@@ -86,31 +87,48 @@ def extract_vesicles(raw_file, seg_file, correction_file, apply_closing=True):
         print("Create vesicle segmentation from boundary annotations")
         seg = make_instances(boundaries, apply_closing=apply_closing)
 
-    v = napari.Viewer()
-    v.add_image(raw)
-    v.add_labels(boundaries)
-    v.add_labels(seg)
-    napari.run()
+    if output_path is None:
+        v = napari.Viewer()
+        v.add_image(raw)
+        v.add_labels(boundaries)
+        v.add_labels(seg)
+        napari.run()
+    else:
+        with open_file(output_path, "a") as f:
+            f.create_dataset("raw", data=raw, compression="gzip")
+            f.create_dataset("labels/vesicles", data=seg, compression="gzip")
+            f.create_dataset("labels/vescile_boundaries", data=boundaries, compression="gzip")
 
 
 def process_33K_L1(output_root):
     raw_file = "/home/pape/Work/data/fernandez-busnadiego/tomos_anotated_18924/33K/L1_ts_002_newstack_rec_deconv_bin4_260224.mrc"  # noqa
     seg_file = "/home/pape/Work/data/fernandez-busnadiego/tomos_anotated_18924/33K/L1_ts_002_SV_bin4_2622024.mrc"  # noqa
     correction_file = "vesicles-33K-L1.tif"
-    extract_vesicles(raw_file, seg_file, correction_file)
+
+    fname = Path(correction_file).stem
+    output_path = None if output_root is None else os.path.join(output_root, f"{fname}.h5")
+    extract_vesicles(raw_file, seg_file, correction_file, output_path=output_path)
 
 
 def process_64K_LAM12(output_root):
     raw_file = "/home/pape/Work/data/fernandez-busnadiego/tomos_anotated_18924/64K/Lam12_ts_006_newstack_rec_deconv_bin4_250823.mrc"  # noqa
     seg_file = "/home/pape/Work/data/fernandez-busnadiego/tomos_anotated_18924/64K/Lam12_ts_006_SV_bin4_250823.mrc"  # noqa
     correction_file = "vesicles-64K-LAM12.tif"
-    extract_vesicles(raw_file, seg_file, correction_file, apply_closing=False)
+
+    fname = Path(correction_file).stem
+    output_path = None if output_root is None else os.path.join(output_root, f"{fname}.h5")
+    extract_vesicles(raw_file, seg_file, correction_file, apply_closing=False, output_path=output_path)
 
 
 def main():
-    output_root = ""
-    # process_33K_L1(output_root)
-    process_64K_LAM12(output_root)
+    # output_root = None
+    output_root = "/home/pape/Work/data/fernandez-busnadiego/vesicle_gt/v1"
+
+    if output_root is not None and not os.path.exists(output_root):
+        os.makedirs(output_root, exist_ok=True)
+
+    process_33K_L1(output_root)
+    # process_64K_LAM12(output_root)
 
 
 if __name__ == "__main__":
