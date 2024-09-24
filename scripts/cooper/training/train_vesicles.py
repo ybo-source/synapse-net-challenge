87edf43766d3c84b1e66feb46405b36efa15258d
SarahMuth <sarahmuth9@gmail.com>
2024-09-24 11:28:11 +0200

    finish training

--- scripts/cooper/training/train_vesicles.py
+++ scripts/cooper/training/train_vesicles.py
@@ -1,20 +1,71 @@
 import os
 from glob import glob
+import argparse
+import json
+
+from sklearn.model_selection import train_test_split
 
 from synaptic_reconstruction.training import supervised_training
 from synaptic_reconstruction.training import semisupervised_training
 
-def train_check():
-    """Training on a subset of the old version of training data to
-    check that the training implementation works.
+TRAIN_ROOT = "/mnt/lustre-emmy-hdd/projects/nim00007/data/synaptic-reconstruction/cooper/vesicles_processed"
+OUTPUT_ROOT = "/scratch-emmy/usr/nimsmuth/synapse_seg/data/training"
 
-    NOTE: patch_shape is chosen arbitrarily, please check what we have used for the
-    earlier training.
-    """
 
-    root = "/scratch-grete/projects/nim00007/data/synaptic_reconstruction/train_data_cooper"
-    train_paths = glob(os.path.join(root, "01_hoi_maus_2020_incomplete", "*.h5"))
-    val_paths = glob(os.path.join(root, "02_hcc_nanogold", "*.h5"))
+def _require_train_val_test_split(datasets):
+    train_ratio, val_ratio, test_ratio = 0.8, 0.1, 0.1
+
+    def _train_val_test_split(names):
+        print(names)
+        train, test = train_test_split(names, test_size=1 - train_ratio, shuffle=True)
+        _ratio = test_ratio / (test_ratio + val_ratio)
+        val, test = train_test_split(test, test_size=_ratio)
+        return train, val, test
+
+    for ds in datasets:
+        print(ds)
+        split_path = os.path.join(OUTPUT_ROOT, f"split-{ds}.json")
+        if os.path.exists(split_path):
+            continue
+
+        file_paths = sorted(glob(os.path.join(TRAIN_ROOT, ds, "*.h5")))
+        print(file_paths)
+        file_names = [os.path.basename(path) for path in file_paths]
+        print(file_names)
+        train, val, test = _train_val_test_split(file_names)
+
+        with open(split_path, "w") as f:
+            json.dump({"train": train, "val": val, "test": test}, f)
+
+
+def get_paths(split, datasets):
+    _require_train_val_test_split(datasets)
+
+    paths = []
+    for ds in datasets:
+        split_path = os.path.join(OUTPUT_ROOT, f"split-{ds}.json")
+        with open(split_path) as f:
+            names = json.load(f)[split]
+        ds_paths = [os.path.join(TRAIN_ROOT, ds, name) for name in names]
+        assert all(os.path.exists(path) for path in ds_paths)
+        paths.extend(ds_paths)
+
+    return paths
+
+def train(key):
+
+    datasets = [
+    "01_hoi_maus_2020_incomplete",
+    "02_hcc_nanogold",
+    "03_hog_cs1sy7",
+    "05_stem750_sv_training",
+    "07_hoi_s1sy7_tem250_ihgp",
+    "10_tem_single_release",
+    "11_tem_multiple_release",
+    "12_chemical_fix_cryopreparation"
+]
+    train_paths = get_paths("train", datasets=datasets)
+    val_paths = get_paths("val", datasets=datasets)
 
     print("Start training with:")
     print(len(train_paths), "tomograms for training")
@@ -25,10 +76,10 @@ def train_check():
     check = False
 
     supervised_training(
-        name="vesicles-model-new_postprocessing",
+        name=f"vesicles-model-new_postprocessing_{key}",
         train_paths=train_paths,
         val_paths=val_paths,
-        label_key="/labels/vesicles_postprocessed",
+        label_key=f"/labels/vesicles/{key}",
         patch_shape=patch_shape, batch_size=batch_size,
         n_samples_train=None, n_samples_val=25,
         check=check,
@@ -37,7 +88,10 @@ def train_check():
 
 
 def main():
-    train_check()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-k", "--key", required=True)
+    args = parser.parse_args()
+    train(args.key)
 
 
 if __name__ == "__main__":
