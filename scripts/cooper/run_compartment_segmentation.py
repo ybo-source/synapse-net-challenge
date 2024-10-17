1b777bf82fa86ececacacad9aabf40639a01c0e7
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-10-17 21:44:11 +0200

    Update compartment segmentation

--- scripts/cooper/run_compartment_segmentation.py
+++ scripts/cooper/run_compartment_segmentation.py
@@ -1,47 +1,51 @@
-import h5py
-import napari
+import argparse
+from functools import partial
 
 from synaptic_reconstruction.inference.compartments import segment_compartments
+from synaptic_reconstruction.inference.util import inference_helper, parse_tiling
 
 
-def check_2d():
-
-    input_path = "synapse-examples/36859_J1_66K_TS_CA3_PS_26_rec_2Kb1dawbp_crop.h5"
-    model_path = "synapse-examples/compartment_model.pt"
-
-    with h5py.File(input_path, "r") as f:
-        input_ = f["raw"][279]
-
-    seg, pred = segment_compartments(
-        input_, model_path=model_path, verbose=True, return_predictions=True, scale=(0.25, 0.25),
-        tiling={"tile": {"x": 512, "y": 512, "z": 1}, "halo": {"x": 32, "y": 32, "z": 1}}
+def run_compartment_segmentation(args):
+    tiling = parse_tiling(args.tile_shape, args.halo)
+    segmentation_function = partial(
+        segment_compartments, model_path=args.model_path, verbose=False, tiling=tiling, scale=[0.25, 0.25, 0.25]
     )
-
-    v = napari.Viewer()
-    v.add_image(input_)
-    v.add_image(pred)
-    v.add_labels(seg)
-    napari.run()
-
-
-def check_3d():
-
-    input_path = "synapse-examples/36859_J1_66K_TS_CA3_PS_26_rec_2Kb1dawbp_crop.h5"
-    model_path = "./synapse-examples/compartment_model_3d.pt"
-
-    with h5py.File(input_path, "r") as f:
-        input_ = f["raw"][:]
-
-    seg, pred = segment_compartments(
-        input_, model_path=model_path, verbose=True, return_predictions=True, scale=(0.25, 0.25, 0.25),
+    inference_helper(
+        args.input_path, args.output_path, segmentation_function, force=args.force, data_ext=args.data_ext
     )
-    with h5py.File("./synapse-examples/distance_prediction.h5", "a") as f:
-        f.create_dataset("pred", data=pred, compression="gzip")
 
 
 def main():
-    # check_2d()
-    check_3d()
+    parser = argparse.ArgumentParser(description="Segment synaptic compartments in EM tomograms.")
+    parser.add_argument(
+        "--input_path", "-i", required=True,
+        help="The filepath to mrc file or directory containing the tomogram data."
+    )
+    parser.add_argument(
+        "--output_path", "-o", required=True,
+        help="The filepath to directory where the segmentation will be saved."
+    )
+    parser.add_argument(
+        "--model_path", "-m", required=True, help="The filepath to the compartment model."
+    )
+    parser.add_argument(
+        "--force", action="store_true",
+        help="Whether to over-write already present segmentation results."
+    )
+    parser.add_argument(
+        "--tile_shape", type=int, nargs=3,
+        help="The tile shape for prediction. Lower the tile shape if GPU memory is insufficient."
+    )
+    parser.add_argument(
+        "--halo", type=int, nargs=3,
+        help="The halo for prediction. Increase the halo to minimize boundary artifacts."
+    )
+    parser.add_argument(
+        "--data_ext", default=".mrc", help="The extension of the tomogram data. By default .mrc."
+    )
+
+    args = parser.parse_args()
+    run_compartment_segmentation(args)
 
 
 if __name__ == "__main__":
--- synaptic_reconstruction/inference/compartments.py
+++ synaptic_reconstruction/inference/compartments.py
@@ -4,32 +4,93 @@ from typing import Dict, List, Optional, Tuple, Union
 import numpy as np
 import torch
 
-from scipy.ndimage import distance_transform_edt
+from scipy.ndimage import distance_transform_edt, binary_closing
 from skimage.measure import label, regionprops
 from skimage.segmentation import watershed
+from skimage.morphology import remove_small_holes
 
 from synaptic_reconstruction.inference.util import get_prediction, _Scaler
 
 
-def _segment_compartments(prediction, boundary_threshold, seed_distance, filter_distance, verbose):
+def _segment_compartments(
+    prediction,
+    boundary_threshold=0.4,  # Threshold for the boundary distance computation.
+    large_seed_distance=30,  # The distance threshold for computing large seeds (= components).
+    small_seed_distance=4,   # The distance threshold for small seeds that will be filtered in the result.
+    verbose=False,
+):
     t0 = time.time()
-    distances = distance_transform_edt(prediction < boundary_threshold)
-    seeds = label(distances > seed_distance)
-    segmentation = watershed(prediction, markers=seeds)
 
-    # Filter by max distance.
-    props = regionprops(segmentation, distances)
-    filter_ids = [prop.label for prop in props if prop.max_intensity < filter_distance]
-    segmentation[np.isin(segmentation, filter_ids)] = 0
+    # Compute the boundary distances.
+    distances = distance_transform_edt(prediction < boundary_threshold)
+
+    # Compute the large seeds. These are the seeds for the compartments to be kept.
+    seeds = label(distances > large_seed_distance)
+
+    # Compute additional small seeds. These are added so that the large seeds don't
+    # flood over boundaries, but will be filtered from the segmentation result.
+    ndim = distances.ndim
+    if ndim == 2:
+        small_seeds = label(distances > small_seed_distance)
+    elif ndim == 3:  # In the 3d case we compute individual seeds per slice.
+        small_seeds = np.zeros_like(seeds)
+        offset = 0
+        for z in range(small_seeds.shape[0]):
+            this_seeds = label(distances[z] > small_seed_distance)
+            this_seeds[this_seeds != 0] += offset
+            offset = this_seeds.max()
+            small_seeds[z] = this_seeds
+    else:
+        raise RuntimeError
+
+    # We only keep small seeds that don't intersect with a large seed.
+    props = regionprops(small_seeds, seeds)
+    keep_seeds = [prop.label for prop in props if prop.max_intensity == 0]
+    keep_mask = np.isin(small_seeds, keep_seeds)
+
+    # Add up the small seeds we keep with the large seeds.
+    seed_offset = seeds.max()
+    seeds[keep_mask] = (small_seeds[keep_mask] + seed_offset)
+
+    # Get the initial segmentation via watershed.
+    raw_segmentation = watershed(prediction, markers=seeds)
+
+    # Thee are the large seed ids that we will keep.
+    keep_ids = list(range(1, seed_offset + 1))
+
+    # Structure lement for 2d dilation in 3d.
+    structure_element = np.ones((3, 3))  # 3x3 structure for XY plane
+    structure_3d = np.zeros((1, 3, 3))  # Only applied in the XY plane
+    structure_3d[0] = structure_element
+
+    # Iterate over the ids, only keep large seeds and remove holes in their respective masks.
+    props = regionprops(raw_segmentation)
+    segmentation = np.zeros_like(raw_segmentation)
+    for prop in props:
+        if prop.label not in keep_ids:
+            continue
+
+        # Get bounding box and mask.
+        bb = tuple(slice(start, stop) for start, stop in zip(prop.bbox[:ndim], prop.bbox[ndim:]))
+        mask = raw_segmentation[bb] == prop.label
+
+        # Fill small holes and apply closing.
+        mask = remove_small_holes(mask, area_threshold=500)
+        mask = np.logical_or(binary_closing(mask, iterations=4), mask)
+        if ndim == 3:
+            mask = np.logical_or(binary_closing(mask, iterations=8, structure=structure_3d), mask)
+        segmentation[bb][mask] = prop.label
 
     if verbose:
         print("Segmentation time in", time.time() - t0, "s")
 
+    # For debugging / development.
     # import napari
     # v = napari.Viewer()
     # v.add_image(prediction)
     # v.add_image(distances)
     # v.add_labels(seeds)
+    # v.add_labels(small_seeds)
     # v.add_labels(segmentation)
     # napari.run()
 
@@ -77,16 +138,9 @@ def segment_compartments(
         assert pred.shape[0] == 1
         pred = pred[0]
 
-    # The parameters for the segmentation. The second two probably need to be optimized further.
-    boundary_threshold = 0.5
-    # The distance for thresholding the boundary distacne for computing connected components.
-    seed_distance = 5
-    # Segments with a smaller max distance than this will be filtered.
-    filter_distance = 40
-    seg = _segment_compartments(
-        pred, boundary_threshold=boundary_threshold,
-        filter_distance=filter_distance, seed_distance=seed_distance, verbose=verbose
-    )
+    # Run the compartment segmentation.
+    # We may want to expose some of the parameters here.
+    seg = _segment_compartments(pred, verbose=verbose)
     seg = scaler.rescale_output(seg, is_segmentation=True)
 
     if return_predictions:
