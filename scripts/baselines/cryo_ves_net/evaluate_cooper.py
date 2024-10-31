9aba9f367fde1574ed513d6ee409343c44329917
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-10-31 16:21:41 +0100

    Implement cryo-ves-net evaluation for cooper data

---
+++ scripts/baselines/cryo_ves_net/evaluate_cooper.py
@@ -0,0 +1,96 @@
+import os
+from glob import glob
+
+import h5py
+import numpy as np
+import pandas as pd
+from elf.evaluation.matching import matching
+
+INPUT_ROOT = "/mnt/lustre-emmy-hdd/projects/nim00007/data/synaptic-reconstruction/cooper/vesicles_processed_v2/testsets"  # noqa
+OUTPUT_ROOT = "./predictions/cooper"  # noqa
+
+DATASETS = [
+    "01_hoi_maus_2020_incomplete",
+    "02_hcc_nanogold",
+    "03_hog_cs1sy7",
+    "05_stem750_sv_training",
+    "07_hoi_s1sy7_tem250_ihgp",
+    "10_tem_single_release",
+    "11_tem_multiple_release",
+    "12_chemical_fix_cryopreparation"
+]
+
+
+def evaluate_dataset(ds_name):
+    result_folder = "./results/cooper"
+    os.makedirs(result_folder, exist_ok=True)
+    result_path = os.path.join(result_folder, f"{ds_name}.csv")
+    if os.path.exists(result_path):
+        results = pd.read_csv(result_path)
+        return results
+
+    print("Evaluating ds", ds_name)
+    input_files = sorted(glob(os.path.join(INPUT_ROOT, ds_name, "*.h5")))
+    pred_files = sorted(glob(os.path.join(OUTPUT_ROOT, ds_name, "*.h5")))
+
+    results = {
+        "dataset": [],
+        "file": [],
+        "precision": [],
+        "recall": [],
+        "f1-score": [],
+    }
+    for inf, predf in zip(input_files, pred_files):
+        fname = os.path.basename(inf)
+
+        with h5py.File(inf, "r") as f:
+            gt = f["/labels/vesicles/combined_vesicles"][:]
+        with h5py.File(predf, "r") as f:
+            seg = f["/prediction/vesicles/cryovesnet"][:]
+        assert gt.shape == seg.shape
+
+        scores = matching(seg, gt)
+
+        results["dataset"].append(ds_name)
+        results["file"].append(fname)
+        results["precision"].append(scores["precision"])
+        results["recall"].append(scores["recall"])
+        results["f1-score"].append(scores["f1"])
+
+    results = pd.DataFrame(results)
+    results.to_csv(result_path, index=False)
+    return results
+
+
+def main():
+    all_results = {}
+    for ds in DATASETS:
+        result = evaluate_dataset(ds)
+        all_results[ds] = result
+
+    groups = {
+        "Chemical Fixation": ["12_chemical_fix_cryopreparation"],
+        "Single-Axis-TEM": ["01_hoi_maus_2020_incomplete"],
+        "Dual-Axis-TEM": ["02_hcc_nanogold",
+                          "03_hog_cs1sy7",
+                          "07_hoi_s1sy7_tem250_ihgp",
+                          "10_tem_single_release",
+                          "11_tem_multiple_release"],
+        "STEM": ["05_stem750_sv_training"],
+        "Overall": DATASETS,
+    }
+
+    for name, datasets in groups.items():
+        f1_scores = []
+
+        for ds in datasets:
+            this_f1_scores = all_results[ds]["f1-score"].values.tolist()
+            f1_scores.extend(this_f1_scores)
+
+        mean_f1 = np.mean(f1_scores)
+        std_f1 = np.std(f1_scores)
+        print(name, ":", mean_f1, "+-", std_f1)
+
+
+if __name__ == "__main__":
+    main()
--- scripts/baselines/cryo_ves_net/segment_cooper.py
+++ scripts/baselines/cryo_ves_net/segment_cooper.py
@@ -1,13 +1,42 @@
+import os
+from glob import glob
+
 from common import apply_cryo_vesnet
+
+INPUT_ROOT = "/mnt/lustre-emmy-hdd/projects/nim00007/data/synaptic-reconstruction/cooper/vesicles_processed_v2/testsets"  # noqa
+OUTPUT_ROOT = "./predictions/cooper"  # noqa
+
+RESOLUTIONS = {
+    "01_hoi_maus_2020_incomplete": (1.554, 1.554, 1.554),
+    "02_hcc_nanogold": (1.2, 1.2, 1.2),
+    "03_hog_cs1sy7": (1.24, 1.24, 1.24),
+    "05_stem750_sv_training": (0.868, 0.868, 0.868),
+    "07_hoi_s1sy7_tem250_ihgp": (1.24, 1.24, 1.24),
+    "10_tem_single_release": (1.24, 1.24, 1.24),
+    "11_tem_multiple_release": (1.24, 1.24, 1.24),
+    "12_chemical_fix_cryopreparation": (1.554, 1.554, 1.554)
+}
+
+
+def segment_dataset(ds_name, resolution):
+    input_folder = os.path.join(INPUT_ROOT, ds_name)
+    output_folder = os.path.join(OUTPUT_ROOT, ds_name)
+
+    n_inputs = len(glob(os.path.join(input_folder, "*.h5")))
+    n_outputs = len(glob(os.path.join(output_folder, "*.h5")))
+    if n_inputs == n_outputs:
+        print(ds_name, "is already processed")
+        return
+
+    apply_cryo_vesnet(
+        input_folder, output_folder, pattern="*.h5", input_key="raw", resolution=resolution, nested=True
+    )
 
 
 def main():
-    input_folder = "/mnt/lustre-emmy-hdd/projects/nim00007/data/synaptic-reconstruction/cooper/vesicles_processed_v2/testsets"  # noqa
-    output_folder = "./cryo-vesnet-test2"
-
-    # TODO determine the correct resolution (in angstrom) for each dataset
-    resolution = (10, 10, 10)
-    apply_cryo_vesnet(input_folder, output_folder, pattern="*.h5", input_key="raw", resolution=resolution, nested=True)
+    for ds_name, resolution in RESOLUTIONS.items():
+        resolution = tuple(res * 10 for res in resolution)
+        segment_dataset(ds_name, resolution)
 
 
 if __name__ == "__main__":
