d991be36e864dd1928dcb5cfbb24a5facc03c532
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-11-16 17:41:57 +0100

    Update distance measurement widget

--- setup.py
+++ setup.py
@@ -8,13 +8,12 @@ setup(
     name="synaptic_reconstruction",
     packages=find_packages(exclude=["test"]),
     version=__version__,
-    author="Constantin Pape; Sarah Muth",
+    author="Constantin Pape; Sarah Muth; Luca Freckmann",
     url="https://github.com/computational-cell-analytics/synaptic_reconstruction",
     license="MIT",
     entry_points={
         "console_scripts": [
-            "sr_tools.correct_segmentation = synaptic_reconstruction.tools.segmentation_correction:main",
-            "sr_tools.measure_distances = synaptic_reconstruction.tools.distance_measurement:main",
+            # TODO add segmentation CLI
         ],
         "napari.manifest": [
             "synaptic_reconstruction = synaptic_reconstruction:napari.yaml",
--- synaptic_reconstruction/tools/distance_measure_widget.py
+++ synaptic_reconstruction/tools/distance_measure_widget.py
@@ -2,6 +2,7 @@ import os
 
 import napari
 import napari.layers
+import numpy as np
 import pandas as pd
 
 from napari.utils.notifications import show_info
@@ -39,109 +40,103 @@ class DistanceMeasureWidget(BaseWidget):
         self.viewer = napari.current_viewer()
         layout = QVBoxLayout()
 
-        self.image_selector_name1 = "Segmentation 1"
-        self.image_selector_name2 = "Segmentation 2"
-        # Create the image selection dropdown
-        # TODO: update the names to make it easier to distinguish what is what.
+        self.image_selector_name1 = "Segmentation"
+        self.image_selector_name2 = "Object"
+        # Create the image selection dropdown.
         self.segmentation1_selector_widget = self._create_layer_selector(self.image_selector_name1, layer_type="Labels")
         self.segmentation2_selector_widget = self._create_layer_selector(self.image_selector_name2, layer_type="Labels")
 
-        # create save path
+        # Create advanced settings.
         self.settings = self._create_settings_widget()
 
-        # create buttons
-        self.measure_pairwise_button = QPushButton("Measure Distance Pairwise")
-        self.measure_segmentation_to_object_button = QPushButton("Measure Distance Segmentation to Object")
+        # Create and connect buttons.
+        self.measure_button1 = QPushButton("Measure Distances")
+        self.measure_button1.clicked.connect(self.on_measure_seg_to_object)
 
-        # Connect buttons to functions
-        self.measure_pairwise_button.clicked.connect(self.on_measure_pairwise)
-        self.measure_segmentation_to_object_button.clicked.connect(self.on_measure_segmentation_to_object)
-        # self.load_model_button.clicked.connect(self.on_load_model)
+        self.measure_button2 = QPushButton("Measure Pairwise Distances")
+        self.measure_button2.clicked.connect(self.on_measure_pairwise)
 
-        # Add the widgets to the layout
+        # Add the widgets to the layout.
         layout.addWidget(self.segmentation1_selector_widget)
         layout.addWidget(self.segmentation2_selector_widget)
         layout.addWidget(self.settings)
-        # layout.addWidget(self.measure_pairwise_button)
-        layout.addWidget(self.measure_segmentation_to_object_button)
+        layout.addWidget(self.measure_button1)
+        layout.addWidget(self.measure_button2)
 
         self.setLayout(layout)
 
-    def on_measure_segmentation_to_object(self):
-        segmentation1_data = self._get_layer_selector_data(self.image_selector_name1)
-        segmentation2_data = self._get_layer_selector_data(self.image_selector_name2)
-        if segmentation1_data is None or segmentation2_data is None:
-            show_info("Please choose both segmentation layers.")
-            return
-
-        (distances,
-         endpoints1,
-         endpoints2,
-         seg_ids) = distance_measurements.measure_segmentation_to_object_distances(
-            segmentation=segmentation1_data,
-            segmented_object=segmentation2_data,
-            distance_type="boundary",
-        )
-
-        if self.save_path.text() != "":
-            data = {"label": seg_ids, "distance": distances}
+    def _to_table_data(self, distances, seg_ids, endpoints1=None, endpoints2=None):
+        assert len(distances) == len(seg_ids), f"{distances.shape}, {seg_ids.shape}"
+        if seg_ids.ndim == 2:
+            table_data = {"label1": seg_ids[:, 0], "label2": seg_ids[:, 1], "distance": distances}
+        else:
+            table_data = {"label": seg_ids, "distance": distances}
+        if endpoints1 is not None:
             axis_names = "zyx" if endpoints1.shape[1] == 3 else "yx"
-            data.update({f"begin-{ax}": endpoints1[:, i] for i, ax in enumerate(axis_names)})
-            data.update({f"end-{ax}": endpoints2[:, i] for i, ax in enumerate(axis_names)})
-            data = pd.DataFrame(data)
-            file_path = _save_distance_table(self.save_path.text(), data)
+            table_data.update({f"begin-{ax}": endpoints1[:, i] for i, ax in enumerate(axis_names)})
+            table_data.update({f"end-{ax}": endpoints2[:, i] for i, ax in enumerate(axis_names)})
+        return pd.DataFrame(table_data)
 
-        lines, properties = distance_measurements.create_object_distance_lines(
-            distances=distances,
-            endpoints1=endpoints1,
-            endpoints2=endpoints2,
-            seg_ids=seg_ids
-        )
-
-        # Add the lines layer
+    def _add_lines_and_table(self, lines, properties, table_data, name):
         line_layer = self.viewer.add_shapes(
             lines,
-            name="Distance Lines",
-            shape_type="line",  # Specify the shape type as 'line'
+            name=name,
+            shape_type="line",
             edge_width=2,
             edge_color="red",
-            blending="additive",  # Use 'additive' for blending if needed
+            blending="additive",
+            properties=properties,
         )
-
-        # FIXME: this doesn't work yet
         if add_table is not None:
             add_table(line_layer, self.viewer)
+
+        if self.save_path.text() != "":
+            file_path = _save_distance_table(self.save_path.text(), table_data)
 
         if self.save_path.text() != "":
             show_info(f"Added distance lines and saved file to {file_path}.")
         else:
             show_info("Added distance lines.")
 
-    def on_measure_pairwise(self):
-        if self.image is None:
-            show_info("Please choose a segmentation.")
-            return
-        if self.save_path.value() is None:
-            show_info("Please choose a save path.")
-            return
-        show_info("Not implemented yet.")
-        return
-        # distance_measurements.measure_pairwise_object_distances(
-        #     segmentation=segmentation, distance_type="boundary",
-        #     save_path=self.save_path
-        #     )
-        # lines, properties = distance_measurements.create_distance_lines(
-        #     measurement_path=self.save_path
-        # )
+    def on_measure_seg_to_object(self):
+        segmentation = self._get_layer_selector_data(self.image_selector_name1)
+        object_data = self._get_layer_selector_data(self.image_selector_name2)
 
-        # # Add the lines layer
-        # self.viewer.add_lines(
-        #     lines, name="Distance Lines", visible=True, edge_width=2, edge_color="red", edge_blend="additive"
-        # )
+        (distances,
+         endpoints1,
+         endpoints2,
+         seg_ids) = distance_measurements.measure_segmentation_to_object_distances(
+            segmentation=segmentation, segmented_object=object_data, distance_type="boundary",
+        )
+        lines, properties = distance_measurements.create_object_distance_lines(
+            distances=distances,
+            endpoints1=endpoints1,
+            endpoints2=endpoints2,
+            seg_ids=seg_ids
+        )
+        table_data = self._to_table_data(distances, seg_ids, endpoints1, endpoints2)
+        self._add_lines_and_table(lines, properties, table_data, name="distances")
+
+    def on_measure_pairwise(self):
+        segmentation = self._get_layer_selector_data(self.image_selector_name1)
+
+        (distances,
+         endpoints1,
+         endpoints2,
+         seg_ids) = distance_measurements.measure_pairwise_object_distances(
+            segmentation=segmentation, distance_type="boundary"
+        )
+        lines, properties = distance_measurements.create_pairwise_distance_lines(
+            distances=distances, endpoints1=endpoints1, endpoints2=endpoints2, seg_ids=seg_ids.tolist()
+        )
+        table_data = self._to_table_data(
+            distances=properties["distance"],
+            seg_ids=np.concatenate([properties["id_a"][:, None], properties["id_b"][:, None]], axis=1)
+        )
+        self._add_lines_and_table(lines, properties, table_data, name="pairwise-distances")
 
     def _create_settings_widget(self):
         setting_values = QWidget()
-        # setting_values.setToolTip(get_tooltip("embedding", "settings"))
         setting_values.setLayout(QVBoxLayout())
 
         self.save_path, layout = self._add_path_param(name="Save Table", select_type="file", value="")
