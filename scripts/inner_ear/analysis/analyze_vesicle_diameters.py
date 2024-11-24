97289510790ad1c14192a1d6610cadcc703b8c05
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-11-24 23:15:12 +0100

    Update vesicle diameter analysis

--- scripts/inner_ear/analysis/analyze_vesicle_diameters.py
+++ scripts/inner_ear/analysis/analyze_vesicle_diameters.py
@@ -15,10 +15,7 @@ from common import get_finished_tomos
 sys.path.append("../processing")
 
 
-def aggregate_radii(data_root, table, save_path, get_tab, include_names):
-    if os.path.exists(save_path):
-        return
-
+def aggregate_diameters(data_root, table, save_path, get_tab, include_names, sheet_name):
     radius_table = []
     for _, row in tqdm(table.iterrows(), total=len(table), desc="Collect tomo information"):
         folder = row["Local Path"]
@@ -41,17 +38,20 @@ def aggregate_radii(data_root, table, save_path, get_tab, include_names):
         tab = pd.read_excel(tab_path)
         this_tab = tab[["pool", "radius [nm]"]]
         this_tab.insert(0, "tomogram", [tomo_name] * len(this_tab))
+        this_tab.insert(3, "diameter [nm]", this_tab["radius [nm]"] * 2)
         radius_table.append(this_tab)
 
     radius_table = pd.concat(radius_table)
-    print("Saving table for", len(radius_table), "vesicles to", save_path)
-    radius_table.to_excel(save_path, index=False)
 
-
-def aggregate_radii_imod(data_root, table, save_path, include_names):
+    print("Saving table for", len(radius_table), "vesicles to", save_path, sheet_name)
     if os.path.exists(save_path):
-        return
+        with pd.ExcelWriter(save_path, engine="openpyxl", mode="a") as writer:
+            radius_table.to_excel(writer, sheet_name=sheet_name, index=False)
+    else:
+        radius_table.to_excel(save_path, sheet_name=sheet_name, index=False)
 
+
+def aggregate_diameters_imod(data_root, table, save_path, include_names, sheet_name):
     radius_table = []
     for _, row in tqdm(table.iterrows(), total=len(table), desc="Collect tomo information"):
         folder = row["Local Path"]
@@ -95,12 +95,16 @@ def aggregate_radii_imod(data_root, table, save_path, include_names):
             "tomogram": [tomo_name] * len(radii),
             "pool": [label_names[label_id] for label_id in labels],
             "radius [nm]": radii,
+            "diameter [nm]": 2 * radii,
         })
         radius_table.append(this_tab)
 
     radius_table = pd.concat(radius_table)
-    print("Saving table for", len(radius_table), "vesicles to", save_path)
-    radius_table.to_excel(save_path, index=False)
+    print("Saving table for", len(radius_table), "vesicles to", save_path, sheet_name)
+    radius_table.to_excel(save_path, index=False, sheet_name=sheet_name)
+
+    man_tomos = pd.unique(radius_table.tomogram)
+    return man_tomos
 
 
 def get_tab_semi_automatic(folder):
@@ -141,20 +145,32 @@ def main():
     table = parse_table(table_path, data_root)
 
     all_tomos = get_finished_tomos()
-    aggregate_radii(
-        data_root, table, save_path="./results/vesicle_radii_semi_automatic.xlsx", get_tab=get_tab_semi_automatic,
-        include_names=all_tomos
+
+    print("All tomograms")
+    save_path = "./results/vesicle_diameters_all_tomos.xlsx"
+    aggregate_diameters(
+        data_root, table, save_path=save_path, get_tab=get_tab_semi_automatic, include_names=all_tomos,
+        sheet_name="Semi-automatic",
+    )
+    aggregate_diameters(
+        data_root, table, save_path=save_path, get_tab=get_tab_proofread, include_names=all_tomos,
+        sheet_name="Proofread",
     )
 
-    aggregate_radii(
-        data_root, table, save_path="./results/vesicle_radii_proofread.xlsx", get_tab=get_tab_proofread,
-        include_names=all_tomos
+    print()
+    print("Tomograms with manual annotations")
+    # aggregate_diameters(data_root, table, save_path="./results/vesicle_radii_manual.xlsx", get_tab=get_tab_manual)
+    save_path = "./results/vesicle_diameters_tomos_with_manual_annotations.xlsx"
+    man_tomos = aggregate_diameters_imod(
+        data_root, table, save_path=save_path, include_names=all_tomos, sheet_name="Manual",
     )
-
-    # aggregate_radii(data_root, table, save_path="./results/vesicle_radii_manual.xlsx", get_tab=get_tab_manual)
-    aggregate_radii_imod(
-        data_root, table, save_path="./results/vesicle_radii_manual.xlsx",
-        include_names=all_tomos
+    aggregate_diameters(
+        data_root, table, save_path=save_path, get_tab=get_tab_semi_automatic, include_names=man_tomos,
+        sheet_name="Semi-automatic",
+    )
+    aggregate_diameters(
+        data_root, table, save_path=save_path, get_tab=get_tab_proofread, include_names=man_tomos,
+        sheet_name="Proofread",
     )
 
 
