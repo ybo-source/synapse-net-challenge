4b3b539d6f0b1f15164e459c4856954036e60ece
Constantin Pape <constantin.pape@informatik.uni-goettingen.de>
2024-12-07 20:50:25 +0100

    Bump version; add outline for analysis pipeline

---
+++ examples/analysis_pipeline.py
@@ -0,0 +1,7 @@
+# TODO implement analysis pipeline for our tomo sample data:
+# - segment vesicles, AZ, and compartment
+# - use compartment segmentation to find the presynaptic terminal
+# - postprocess the AZ segmentation
+# - measure distances between vesicles and AZ
+# - vesicle pool assignment into docked and non-attached terminals
+# - export table with distance and morphology measurements for the two pools
